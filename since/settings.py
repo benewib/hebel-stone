f2c0c66c526b83b6129e49dd919df1775c23532d
Benny Wibowo <benewib@gmail.com>
2016-09-05 18:00:26 +0700

    Hebel Stone beta 2

--- since/settings.py
+++ since/settings.py
@@ -126,8 +126,6 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
